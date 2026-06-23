608780f4b7f68eb3c585845ad44608bbfb100ce4
venera081 <berekettop0010@gmail.com>
2026-06-23 15:47:54 +0600

    Change routers and add forms.user.py

--- handlers/routers.py
+++ handlers/routers.py
@@ -10,50 +10,57 @@ from aiogram.types import (
     InlineKeyboardMarkup,
     InlineKeyboardButton
 )
-
+from forms.user import Form
+from aiogram.fsm.context import FSMContext
 
 router = Router()
 
-def get_main_reply_keyboard():
-    keyboard = ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="AboutBot")],
-            [KeyboardButton(text="Start"), KeyboardButton(text="Help")]
-        ],
-        resize_keyboard=True
-    )
+@router.message(Command("start"))
+async def start(message: Message, state: FSMContext):
+    await message.answer("Let's start filling in the form!\nFirst of all enter your name:")
+    await state.set_state(Form.name)
 
-    return keyboard
+@router.message(Command("cancel"))
+async def cancel_form(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer("Bio canceled!")
 
-def get_main_inline_keyboard():
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="Open website", url="https://www.bbc.com/news")]
-            [InlineKeyboardButton(text="learn more", callback_data="info_more")]
-        ]
-    )
+@router.message(Form.name, F.text)
+async def proccess_name(message: Message, state:FSMContext):
+    await state.update_data(name=message.text)
 
-    return keyboard
+    await message.answer("Amazing!\n And enter your age:")
+    await state.set_state(Form.age)
 
-@router.callback_query(lambda c: c.data == "info_more")
-async def proccess_more_info(callback: CallbackQuery):
-    await callback.message.answer("More detail info....")
-    await callback.answer()
-
-@router.message(CommandStart())
-@router.message(F.text.lower() == "старт")
-async def start(message: Message):
-    await message.answer(
-        "Hi I'm bot for _news_. \n\n🌍 *Choose* *language* in your keyboard",
-          reply_markup=lang_kb,
-          parse_mode="Markdown",
-          reply_markup=get_main_inline_keyboard())
-
-@router.message(Command("help"))
-async def help(message: Message):
-    await message.answer("Commands:\n/start - launch bot\n/help - list of commands",
-                         reply_markup=get_main_reply_keyboard())
+@router.message(Form.age, F.text)
+async def proccess_age(message: Message, state:FSMContext):
+    if not message.text.isdigit():
+        await message.answer("Age must be digit")
+        return
+        
+    if int(message.text) < 12 or int(message.text) > 100:
+        await message.answer("Age must be from 12 to 100")
+        return
     
-@router.message()
-async def misunderstand(message: Message):
-    await message.answer("Sorry, I can't understand write /help to watch comands")
+    await state.update_data(age=int(message.text))
+
+    await message.answer("Excellent!\n Then enter your email:")
+    await state.set_state(Form.email)
+
+@router.message(Form.email, F.text)
+async def proccess_email(message: Message, state:FSMContext):
+    email_text = message.text
+    if "@" not in email_text or "gmail.com" not in email_text:
+        await message.answer("Email doesn't correct")
+        return
+    
+    await state.update_data(email=email_text)
+
+    data = await state.get_data()
+    name = data["name"]
+    age = data["age"]
+    email = data["email"]
+
+    await message.answer(f"Your bio is ready\nName: {name}\nAge: {age}\nEmail: {email}")
+    await state.clear()
+
